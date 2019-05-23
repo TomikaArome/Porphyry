5f2140d99f762dae13b480e305902a9b49ba705e
Thomas Hunter <tommychasseur@gmail.com>
2019-05-24 00:59:22 +0200

    FEATURE: Select mode added

--- src/components/Corpora/Corpora.jsx
+++ src/components/Corpora/Corpora.jsx
@@ -10,18 +10,43 @@ let listView = getConfig('listView', {
 });
 
 class Corpora extends Component {
+  
+  constructor() {
+	  super();
+	  this.state = {
+		selectMode: false,
+		selectedItems: {}
+	  };
+  }
 
   render() {
     let items = this._getItems();
     let count = this.props.items.length;
     let total = this.props.from;
+	
+	let subjectH2;
+	if (this.state.selectMode) {
+	  subjectH2 = (
+	    <h2 className="h4 font-weight-bold text-center">
+		  <span>Mode sélection</span>
+		  <div><button className="btn btn-light ml-4 mr-2" onClick={_ => console.log("click")}>Attribuer un topic</button></div>
+		  <div><button className="btn btn-danger" onClick={_ => this.setState({selectMode: false})}>Annuler</button></div>
+		</h2>
+	  );
+	} else {
+	  subjectH2 = (
+	    <h2 className="h4 font-weight-bold text-center">
+          <span>{this.props.ids.join(' + ')}</span>
+          <span className="badge badge-pill badge-light ml-4 mr-2">{count} / {total}</span>
+		  <div><button className="btn btn-light" onClick={_ => this.setState({selectMode: true})}>Sélectionner</button></div>
+        </h2>
+	  );
+	}
+	
     return(
       <div className="col-md-8 p-4">
         <div className="Subject">
-          <h2 className="h4 font-weight-bold text-center">
-            {this.props.ids.join(' + ')}
-            <span className="badge badge-pill badge-light ml-4">{count} / {total}</span>
-          </h2>
+		  {subjectH2}
           <div className="Items m-3">
             {items}
           </div>
@@ -32,9 +57,15 @@ class Corpora extends Component {
 
   _getItems() {
     return this.props.items.map(item =>
-        <Item key={item.id} item={item}
+        <Item corpora={this} key={item.id} item={item}
           id={item.corpus+'/'+item.id} />
     );
+  }
+  
+  _toggleItemState(item) {
+	let temp = this.state.selectedItems;
+	temp[item.id] = !temp[item.id];
+	this.setState({ selectedItems: temp });
   }
 
 }
@@ -42,11 +73,11 @@ class Corpora extends Component {
 function Item(props) {
   switch (listView.mode) {
   case 'article':
-    return Article(props.item);
+    return Article(props);
   case 'picture':
-    return Picture(props.item);
+    return Picture(props);
   default:
-    return Picture(props.item);
+    return Picture(props);
   }
 }
 
@@ -57,13 +88,13 @@ function getString(obj) {
   return String(obj);
 }
 
-function Article(item) {
+function Article(props) {
   let propList = (listView.props || []).map(key => {
-    return <li>{key} : <strong>{getString(item[key])}</strong></li>;
+    return <li>{key} : <strong>{getString(props.item[key])}</strong></li>;
   });
 
-  let uri = `/item/${item.corpus}/${item.id}`;
-  let name = getString(item[listView.name]);
+  let uri = `/item/${props.item.corpus}/${props.item.id}`;
+  let name = getString(props.item[listView.name]);
   return (
     <div className="Article">
       <div className="ArticleTitle"><Link to={uri}>{name}</Link></div>
@@ -72,18 +103,29 @@ function Article(item) {
   );
 }
 
-function Picture(item) {
-  let uri = `/item/${item.corpus}/${item.id}`;
-  let img = getString(item[listView.image]);
-  let name = getString(item[listView.name]);
-  return (
-    <div className="Item">
-      <Link to={uri}>
+function Picture(props) {
+  let uri = `/item/${props.item.corpus}/${props.item.id}`;
+  let img = getString(props.item[listView.image]);
+  let name = getString(props.item[listView.name]);
+  let selected = props.corpora.state.selectedItems[props.item.id];
+  if (props.corpora.state.selectMode) {
+	return (
+	  <div className={selected ? "Item selected" : "Item"} onClick={_ => props.corpora._toggleItemState(props.item)}>
+	    <span className="oi oi-check"></span>
         <img src={img} alt={name}/>
-      </Link>
-      <div className="text-center">{name}</div>
-    </div>
-  );
+        <div className="text-center">{name}</div>
+      </div>
+	);
+  } else {
+    return (
+      <div className="Item">
+        <Link to={uri}>
+          <img src={img} alt={name}/>
+        </Link>
+        <div className="text-center">{name}</div>
+      </div>
+    );
+  }
 }
 
 export default Corpora;
